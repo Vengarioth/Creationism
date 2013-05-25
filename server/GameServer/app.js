39d7a303daf3172388d4d5fad280ae3777688ac6
Andi Heinrich <andi@wafna.de>
2013-05-25 16:51:22 +0200

    asdf

--- server/GameServer/app.js
+++ server/GameServer/app.js
@@ -1,9 +1,40 @@
+var Player = require('./model/player.js');
+var last_time = 0;
+
 module.exports = new Class({
 
+    player_list: new Array(),
 
-    initialize: function() {
+    initialize: function(user_list) {
+        for (var user in user_list) {
+            var player = new Player(user);
+            player_list.push(player);
+            user.socket.on('inGame_set_choices', player.set_choices(data));
+        }
+        setTimeout(this.update, 100);
+    },
 
+    update: function() {
+        var date = new Date();
+        var ms = date.getMilliseconds();
+        dt = ms - last_time;
+        last_time = ms;
+        for (var player in player_list) {
+            player.do_update(dt);
+        }
+        var scoreboard = new Array();
+        for (var player in player_list) {
+            scoreboard.push(player.dynamicValues.population);
+        }
+        for (var player in player_list) {
+            this.send_to_client(player, scoreboard);
+        }
+    },
+
+    send_to_client: function(player, scoreboard) {
+        console.log(player.dynamicValues);
+        player.user.socket.emit('inGame_update', player.dynamicValues);
+        player.user.socket.emit('inGame_scoreboard', scoreboard);
     }
-
-
 });
+
---
+++ server/GameServer/model/player.js
@@ -0,0 +1,41 @@
+GROWTH_FACTOR = 1.0;
+
+module.exports = new Class({
+
+    user: null,
+
+    choices: {
+        religion: 0,
+        government: 0
+    },
+
+    staticValues: {
+        gravity: 0,
+        water: 0
+    },
+
+    dynamicValues: {
+        population: 100000,
+        stability: 0,
+        happiness: 0,
+        technology: 0,
+        growth: 0,
+        pollution: 0,
+        energy: 0
+    },
+
+    initialize: function(user) {
+        this.user = user;
+        this.staticValues.gravity = Math.random();
+        this.staticValues.water = Math.random();
+    },
+
+    set_choices: function(new_choices) {
+        this.choices = new_choices;
+    },
+
+    do_update: function(dt) {
+        this.dynamicValues.population *= this.dynamicValues.growth * GROWTH_FACTOR * dt;
+    }
+
+});
